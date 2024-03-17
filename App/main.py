537178c9bed2b033df822889466783312fbd8038
andresfdezvg <158042833+andresfdezvg@users.noreply.github.com>
2024-03-17 16:15:51 +0100

    Descarga de datos en linea

--- App/main.py
+++ App/main.py
@@ -1,4 +1,5 @@
 # Main file
+from App.Descarga_datos.download_data import descargar_descomprimir_y_guardar
 from App.POC.Poc_ETL import poc_etl
 from App.POC.Poc_modelado import poc_modelado
 
@@ -8,5 +9,13 @@ def poc():
     poc_modelado()
 
 
+def download_data():
+    # Nombre del conjunto de datos en Kaggle
+    nombre_conjunto_datos = 'davidcariboo/player-scores'  # Reemplaza con el nombre del conjunto que deseas descargar
+    descargar_descomprimir_y_guardar(nombre_conjunto_datos)
+
+
 if __name__ == '__main__':
-    poc()
+    #poc()
+    download_data()
+

0f31344b3e490788db79b3cd942dfbc90b852eba
Felipe Veliz <f.veliz.sanchez@gmail.com>
2024-04-01 20:18:38 +0200

    test para load data warehouse

--- Code/App/Model/insert_data_into_warehouse.py
+++ Code/App/Model/insert_data_into_warehouse.py
@@ -1,6 +1,33 @@
+import psycopg2
+import pandas as pd
+
 # Archivo para configurar los datos del warehouse e insertarlos
 def load_data_into_warehouse_from_df(df):
-    pass
+
+
+# Conectarse a la base de datos
+    conn = psycopg2.connect(
+    host="tu_host",
+    database="tu_base_de_datos",
+    user="tu_usuario",
+    password="tu_contraseña"
+)
+
+# Crear un cursor para ejecutar consultas SQL
+cur = conn.cursor()
+
+# Ejecutar una consulta para seleccionar los datos específicos de la tabla games
+cur.execute("""
+    SELECT game_id, season, stadium, home_club_name, away_club_id, result1x2
+    FROM games
+""")
+
+# Obtener los datos seleccionados
+data = cur.fetchall()
+
+# Cerrar el cursor y la conexión
+cur.close()
+conn.close()
 
 def insert_data_into_warehouse():
     pass
